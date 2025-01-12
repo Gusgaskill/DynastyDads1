ff63bd29bedd605db556264c9f5b591dbf70c937
Gusgaskill <138895338+Gusgaskill@users.noreply.github.com>
2025-01-12 12:04:11 -0600

    Dynasty Dads 1

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -1,7 +1,7 @@
 /*   STEP 1   */
-export const leagueID = "1062784235112607744"; // your league ID
-export const leagueName = "MFFL"; // your league name
-export const dues = 75; // (optional) used in template constitution page
+export const leagueID = "1181064137580417024"; // your league ID
+export const leagueName = "Dynasty Dads 1"; // your league name
+export const dues = 20; // (optional) used in template constitution page
 export const dynasty = true; // true for dynasty leagues, false for redraft and keeper
 export const enableBlog = false; // requires VITE_CONTENTFUL_ACCESS_TOKEN and VITE_CONTENTFUL_SPACE environment variables
 
